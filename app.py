a00f5dafdb488d4a751f7dd53992385ef30bd037
Adhiansyah Ancha <69331906+adhiansyahancha@users.noreply.github.com>
2023-06-19 11:22:56 +0700

    TODO hari ini

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import requests
 
 # Endpoint API cuaca
-data_cuaca = requests.get("https://cuaca.umkt.ac.id/api/cuaca/DigitalForecast-JawaTengah.xml?format=json").json()['row']['data']['forecast']
+data_cuaca = requests.get("https://cuaca.umkt.ac.id/api/cuaca/DigitalForecast-JawaTengah.xml?format=json").json()['row']['data']['forecast']['area']
