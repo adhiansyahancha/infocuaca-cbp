4341fdd0c09be01433277717d6164fd5870d5c55
rafiyamanaka <rafiyamanaka32@gmail.com>
2023-06-19 12:43:43 +0700

    Tambah validasi input

--- operasi.py
+++ operasi.py
@@ -1,6 +1,25 @@
+import re
 """
 Berkas ini berisi operasi-operasi untuk pengolahan prakiraan cuaca
 """
+def validate_input(input_str):
+    # Contoh inputan
+    # user_input = input("Masukkan input: ")
+
+    # try:
+    #     validated_input = validate_input(user_input)
+    #     print("Kota yang dimasukkan:", validated_input)
+    # except ValueError as e:
+    #     print("Error:", e)
+    
+    input_str = input_str.strip()
+    pattern = r"^[a-zA-Z]+$"
+    if not re.match(pattern, input_str):
+        raise ValueError("Input hanya boleh berisi huruf alfabet")
+
+    return input_str
+
+
 
 def urutkan_kota(array):
     for z in range(len(array)):
