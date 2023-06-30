8a22722ea74f031da5b01a7a50451b30c7de4f87
DCandraPratama <dwicandrapratama4@gmail.com>
2023-06-30 09:29:30 +0700

    memperbaiki sedikit isiannya

--- src/tentang.py
+++
@@ -1,8 +0,0 @@
-import os
-from fungsi import baca_fail
-
-
-def tentang():
-    os.system("cls||clear")
-    print("\r" + baca_fail("kepala.txt") + baca_fail("tentang.txt") + "\n")
-    input("\rMasukkan [Q] untuk keluar ")
